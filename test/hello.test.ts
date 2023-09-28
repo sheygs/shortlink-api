002ff2c7087c90479f4adcb40f632565c4dbf541
Olusegun Ekoh @sheygs13 <segun.ekoh@gmail.com>
2023-09-28 23:14:37 +0100

    chore(test): :test_tube: basic unit test :test_tube:

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -6,7 +6,7 @@ describe('Say Hello', () => {
     expect(result).toEqual('Hello JavaScript');
   });
 
-  it('should return "Hello TypeScript"', () => {
+  it('should return "Hello TypeScript" with no parameter passed', () => {
     const result = sayHello();
     expect(result).toEqual('Hello TypeScript');
   });
