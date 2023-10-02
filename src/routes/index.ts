debe069df671e26db33377221275797acd907fe9
Olusegun Ekoh @sheygs13 <segun.ekoh@gmail.com>
2023-10-02 16:10:30 +0100

    chore(): Refactor rate limiter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,13 @@
 import { Router } from 'express';
-import { rateLimit } from 'express-rate-limit';
-import { invalidRoute, baseRoute } from './base';
 import config from '../config';
 import ShortUrlRouter from './url-shortener';
 import UrlAnalyticsRouter from './url-analytics';
-
-const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minutes
-  limit: 5, // Limit each IP to 5 requests per `window` (here, per 1 minutes)
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: 'You have exceeded the 5 requests per minute limit',
-  keyGenerator: (req, _) => req.ip // Get client's IP
-});
+import { invalidRoute, baseRoute } from './base';
 
 const router = Router();
 
 router.get('/', baseRoute);
-router.use('/api', limiter);
+
 router.use(`/api/${config.VER}/urls`, ShortUrlRouter);
 router.use(`/api/${config.VER}/urls`, UrlAnalyticsRouter);
 router.all('*', invalidRoute);
